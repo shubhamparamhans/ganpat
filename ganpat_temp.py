73de67b28e2fbe648f4e479ea2448e8546d38bbc
Shubham Paramhans <shubhamparamhans@gmail.com>
2013-08-22 11:48:32 +0530

    Cluster

--- ganpat_temp.py
+++ ganpat_temp.py
@@ -1,6 +1,7 @@
 import time
 from parser import parser
 from downloader import downloader
+from ClusterConnect import ClusterConnect
 
 class ganpat:
     def __init__(self,site_name,site_url):
@@ -18,9 +19,17 @@ class ganpat:
     			if parse.parse():
     				parse_links=parse.links_are();
     				parse_links=self.check_rel(parse_links,"http://");
-    				self.remove_duplicate()
+    				self.remove_duplicate(1,parse_links);
+    				parse_img=parse.images_are();
+    				self.remove_duplicate(2,parse_img);
     	else:
     		print "Error in downloading %s. Now exitting"%(self._siteurl);
     		sys.exit(0);
+
+    def check_rel(self,links,_rel):
+    	for alink in links:
+    		if not(alink.__contains__(rel)):
+    			alink=self._siteurl+alink;
+    	return links;
 
 
